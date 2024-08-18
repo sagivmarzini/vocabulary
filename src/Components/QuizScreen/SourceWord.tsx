d2084ab7c9f0c29d581cb009a46e9172101ae9a2
sagivmarzini <sagivmarzini1@gmail.com>
2024-08-18 23:55:04 +0300

    add reveal animation for the question text

--- src/Components/QuizScreen/SourceWord.tsx
+++ src/Components/QuizScreen/SourceWord.tsx
@@ -9,13 +9,21 @@ export default function SourceWord({ children}: Props) {
   const textRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
+    // Fade-in from bottom animation
+    // textRef.current?.animate([
+    //   {transform: 'translateY(50%)', opacity: 0, /* letterSpacing: '5px' */},
+    //   {tranform: 'none'}
+    // ], {
+    //   duration: 300,
+    //   easing: 'ease'
+    // })
     textRef.current?.animate([
-      {transform: 'translateY(50%)', opacity: 0, /* letterSpacing: '5px' */},
-      {tranform: 'none'}
-    ], {
-      duration: 300,
-      easing: 'ease'
-    })
+        {transform: 'translateY(100%)'},
+        {transform: 'none'}
+      ], {
+        duration: 500,
+        easing: 'ease'
+      })
   }, [children])
 
   return (
