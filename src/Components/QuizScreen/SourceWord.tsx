2723dcc55a8e0e121d42419a4147532f781da30f
sagivmarzini <sagivmarzini1@gmail.com>
2024-08-19 13:20:26 +0300

    question word plays appear animation when clicked on

--- src/Components/QuizScreen/SourceWord.tsx
+++ src/Components/QuizScreen/SourceWord.tsx
@@ -9,20 +9,24 @@ export default function SourceWord({ children}: Props) {
   const textRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    textRef.current?.animate([
-        {transform: 'translateY(100%)'},
-        {transform: 'none'}
-      ], {
-        duration: 500,
-        easing: 'ease'
-      })
+    playAppearAnimation(textRef.current)
   }, [children])
+
+  function playAppearAnimation(element: HTMLElement | null) {
+    element?.animate([
+      {transform: 'translateY(100%)'},
+      {transform: 'none'}
+    ], {
+      duration: 500,
+      easing: 'ease'
+    })
+  }
 
   return (
     <>
         <div className='word-container'>
           <div className="word-mask">
-            <div className="word" ref={textRef} >{children}</div>
+            <div className="word" ref={textRef} onClick={() => playAppearAnimation(textRef.current)}>{children}</div>
           </div>
         </div>
     </>
