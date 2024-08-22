e5a1fe3fac740fce95a9d5c56c07a66867d40c86
sagivmarzini <sagivmarzini1@gmail.com>
2024-08-22 13:05:05 +0300

    Update Quiz.tsx

--- src/Components/Quiz.tsx
+++ src/Components/Quiz.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useMemo, useRef, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import { Word } from "../types";
 import Choices from "./QuizScreen/Choices/Choices";
 import ProgressBar from "./QuizScreen/ProgressBar";
@@ -54,43 +54,40 @@ export default function Quiz({ initialWords, resetGame }: Props) {
   const levelUpSound = useRef(new Audio("./sounds/level-up.mp3"));
 
   useEffect(() => {
-    localStorage.setItem("quizProgress", JSON.stringify(gameState));
-  }, [gameState]);
-
-  const nextWord = useCallback(
-    (index: number) => {
-      const currentWord = words[index];
-      const isHebrew = Math.random() < 0.5;
-
-      const sourceWord = isHebrew ? currentWord.hebrew : currentWord.arabic;
-      const correctAnswer = isHebrew ? currentWord.arabic : currentWord.hebrew;
-
-      // Populate the question choices
-      const newChoices = [correctAnswer];
-      while (newChoices.length < NUM_CHOICES) {
-        const randomWord = words[Math.floor(Math.random() * words.length)];
-        const choice = isHebrew ? randomWord.arabic : randomWord.hebrew;
-        if (!newChoices.includes(choice)) {
-          newChoices.push(choice);
-        }
-      }
-
-      setGameState((prevState: GameState) => ({
-        ...prevState,
-        sourceWord,
-        correctAnswer,
-        choices: shuffleArray(newChoices),
-        blockAnswering: false,
-      }));
-    },
-    [words]
-  );
-
-  useEffect(() => {
     if (words.length > 0) {
       nextWord(gameState.currentWordIndex);
     }
-  }, [words, gameState.currentWordIndex, nextWord]);
+  }, [words]);
+
+  useEffect(() => {
+    localStorage.setItem("quizProgress", JSON.stringify(gameState));
+  }, [gameState]);
+
+  function nextWord(index: number) {
+    const currentWord = words[index];
+    const isHebrew = Math.random() < 0.5;
+
+    const sourceWord = isHebrew ? currentWord.hebrew : currentWord.arabic;
+    const correctAnswer = isHebrew ? currentWord.arabic : currentWord.hebrew;
+
+    // Populate the question choices
+    const newChoices = [correctAnswer];
+    while (newChoices.length < NUM_CHOICES) {
+      const randomWord = words[Math.floor(Math.random() * words.length)];
+      const choice = isHebrew ? randomWord.arabic : randomWord.hebrew;
+      if (!newChoices.includes(choice)) {
+        newChoices.push(choice);
+      }
+    }
+
+    setGameState((prevState: GameState) => ({
+      ...prevState,
+      sourceWord,
+      correctAnswer,
+      choices: shuffleArray(newChoices),
+      blockAnswering: false,
+    }));
+  }
 
   function checkAnswer(event: React.MouseEvent<HTMLButtonElement>) {
     const userAnswer = event.currentTarget.innerText;
@@ -142,7 +139,7 @@ export default function Quiz({ initialWords, resetGame }: Props) {
       <Choices
         checkAnswer={gameState.blockAnswering ? () => {} : checkAnswer}
         choices={gameState.choices}
-      />
+      ></Choices>
       <button className="reset-btn" onClick={resetGame}>
         איפוס משחק
       </button>
